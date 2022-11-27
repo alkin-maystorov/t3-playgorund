6632d025f0b5b9f6036c256575190325c8c1abec
Alkin <a.maystorov@vehiculum.de>
2022-11-27 14:57:49 +0100

    setup basic auth with discord provider

--- my-t3-app/src/pages/index.tsx
+++ my-t3-app/src/pages/index.tsx
@@ -1,9 +1,25 @@
-import { type NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-const Home: NextPage = () => {
+const Home = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
   return (
     <main>
       <h1>Guestbook</h1>
+      <div>
+        {session ? (
+          <>
+            <p>hi {session.user?.name}</p>
+            <button onClick={() => signOut()}>Logout</button>
+          </>
+        ) : (
+          <button onClick={() => signIn("discord")}>Login with Discord</button>
+        )}
+      </div>
     </main>
   );
 };
