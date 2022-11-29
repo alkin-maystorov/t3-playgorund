a9a5259944d8bdfa2d4cd37e5936f88dd3271aef
Alkin <a.maystorov@vehiculum.de>
2022-11-29 06:33:26 +0100

    add basic styling

--- my-t3-app/src/pages/index.tsx
+++ my-t3-app/src/pages/index.tsx
@@ -4,24 +4,30 @@ const Home = () => {
   const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <main>Loading...</main>;
+    return <main className="flex flex-col items-center pt-4">Loading...</main>;
   }
 
   return (
-    <main>
-      <h1>Guestbook</h1>
-      <div>
-        {session ? (
-          <>
-            <p>hi {session.user?.name}</p>
-            <button onClick={() => signOut()}>Logout</button>
-          </>
-        ) : (
-          <button onClick={() => signIn("discord")}>Login with Discord</button>
-        )}
+    <main className="flex flex-col items-center">
+      <h1 className="pt-4 text-3xl">Guestbook</h1>
+      <p>
+        Practice <code>create-t3-app</code>
+      </p>
+      <div className="pt-10">
+        <div>
+          {session ? (
+            <>
+              <p>hi {session.user?.name}</p>
+              <button onClick={() => signOut()}>Logout</button>
+            </>
+          ) : (
+            <button onClick={() => signIn("discord")}>
+              Login with Discord
+            </button>
+          )}
+        </div>
       </div>
     </main>
   );
 };
-
 export default Home;
